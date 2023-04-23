7dbe69718b2606845682ac7c0688c74fb9fb9f0c
Pat Smith <psmith366@gatech.edu>
2023-04-22 22:39:40 -0400

    Updated functional tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -15,17 +17,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # Header mentions the to-do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is invited to enter to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # User types a to-do item into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # User hitting enter updates the page and lists updated to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # There is a text box inviting entering a second item User enters second item
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # User hitting enter updates the page and lists updated to-do list
+        self.fail('Finish the test!')
 
         # Unique list url is created with some explanation text
 
