459a826eb8eb608b97bdd1b9244107c9fb1cc027
patrick-j-smith <smithpatrick1992@gmail.com>
2026-02-13 20:27:03 -0500

    Updated files

---
+++ functional_tests/tests.py
@@ -0,0 +1,62 @@
+import time
+import unittest
+
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 5
+
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(
+                    row_text,
+                    [row.text for row in rows]
+                )
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+
+    def test_can_start_a_todo_list(self):
+        self.browser.get(self.live_server_url)
+
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        #self.fail("Blah!")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ lists/tests.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from lists.models import Item
+
+
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
+    def test_can_save_a_POST_request(self):
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        first_save_item = saved_items[0]
+        second_save_item = saved_items[1]
+        self.assertEqual(first_save_item.text, "The first (ever) list item")
+        self.assertEqual(second_save_item.text, "Item the second")
