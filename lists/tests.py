68dde2769e1b9b1d9ae533260e8a27a0837359a3
patrick-j-smith <smithpatrick1992@gmail.com>
2026-03-07 21:21:14 -0500

    Updated test cases to account for whitespace and loog at specific DOM objects

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,6 @@
+import lxml.html
 from django.test import TestCase
+
 from lists.models import Item, List
 
 
@@ -10,12 +12,10 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, '<form method="POST" action=/lists/new>')
-        self.assertContains(
-            response,
-            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
-            html=True,
-        )
+        parsed = lxml.html.fromstring(response.content)
+        [form] = parsed.cssselect("form[method=POST]")
+        self.assertEqual(form.get("action"), "/lists/new")
+        [input] = form.cssselect("input[name=item_text]")
 
 
 class NewListTest(TestCase):
@@ -55,15 +55,11 @@ class ListViewTest(TestCase):
     def test_renders_input_form(self):
         mylist = List.objects.create()
         response = self.client.get(f"/lists/{mylist.id}/")
-        self.assertContains(
-            response,
-            f'<form method="POST" action=/lists/{mylist.id}/add_item>'
-        )
-        self.assertContains(
-            response,
-            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item"',
-            html=True
-        )
+        parsed = lxml.html.fromstring(response.content)
+        [form] = parsed.cssselect("form[method=POST]")
+        self.assertEqual(form.get("action"), f"/lists/{mylist.id}/add_item")
+        [input] = form.cssselect("input[name=item_text]")
+
 
     def test_displays_all_list_items(self):
         correct_list = List.objects.create()
