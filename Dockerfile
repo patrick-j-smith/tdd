e3a621962858c869e75d760c61a7bb28f56af3d0
patrick-j-smith <smithpatrick1992@gmail.com>
2026-03-24 04:07:07 -0400

    Add collectstatic to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,15 @@ LABEL authors="patrick-smith"
 RUN python -m venv /venv
 ENV PATH="/venv/bin:$PATH"
 
-RUN pip install "django<6" gunicorn "python-decouple" whitenoise
+COPY requirements.txt /tmp/requirements.txt
+RUN pip install -r tmp/requirements.txt
 
 COPY src /src
-
 WORKDIR /src
 
-#CMD ["python", "manage.py", "runserver", "0.0.0.0:8888"]
+RUN python manage.py collectstatic
+
+ENV DJANGO_DEBUG_FALSE=1
+
 CMD ["gunicorn", "--bind", ":8888", "superlists.wsgi:application"]
+#CMD ["bash"]
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = '*%(8el1wc=r))-q9xu73g$+q&q#m8kj+&&#gv*t98*@+b6x-h7'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
